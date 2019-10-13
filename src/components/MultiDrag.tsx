33223371ea0bc0918e3f013352ce8125c292c815
subal <subal@pixiv.com>
2019-10-13 14:57:42 +0900

    can select layers

--- src/components/MultiDrag.tsx
+++ src/components/MultiDrag.tsx
@@ -1,3 +1,4 @@
+import immer from 'immer'
 import sortBy from 'lodash/sortBy'
 import React, { useReducer } from 'react'
 
@@ -10,10 +11,12 @@ interface Layer {
 }
 
 interface State {
+  selected: Record<Layer['id'], boolean>
   layers: Layer[]
 }
 
-const initialState = {
+const initialState: State = {
+  selected: {},
   layers: [
     { id: '1', x: 10, y: 10, z: 0, color: 'cyan' },
     { id: '2', x: 100, y: 100, z: 1, color: 'orange' },
@@ -21,12 +24,39 @@ const initialState = {
   ]
 }
 
-function reducer(state = initialState, action) {
-  return initialState
+const action = <T extends string, P>(type: T, payload: P) => ({ type, payload })
+
+const LayerActions = {
+  selected: (id: Layer['id']) => action('selected', { id })
 }
+
+const unreduceable = (unknownAction: never) => void unknownAction
+
+type KnownLayerActions = ReturnType<
+  typeof LayerActions[keyof typeof LayerActions]
+>
+
+const reducer = (currentState = initialState, action: KnownLayerActions) =>
+  immer(currentState, state => {
+    switch (action.type) {
+      case 'selected': {
+        const { id } = action.payload
+        state.selected[id] = !state.selected[id]
+        break
+      }
+
+      default: {
+        unreduceable(action.type)
+      }
+    }
+  })
 
 export default function MultiDrag() {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  const onSelect = (id: Layer['id']) => {
+    dispatch(LayerActions.selected(id))
+  }
 
   return (
     <svg
@@ -36,20 +66,39 @@ export default function MultiDrag() {
       style={{ background: '#eee' }}
     >
       {sortBy(state.layers, l => -l.z).map(layer => (
-        <Layer key={layer.id} layer={layer} />
+        <Layer
+          key={layer.id}
+          layer={layer}
+          onSelect={onSelect}
+          selected={!!state.selected[layer.id]}
+        />
       ))}
     </svg>
   )
 }
 
-function Layer({ layer }: { layer: Layer }) {
+function Layer({
+  layer,
+  selected,
+  onSelect
+}: {
+  layer: Layer
+  selected?: boolean
+  onSelect(id: Layer['id']): void
+}) {
+  const onClick = () => {
+    onSelect(layer.id)
+  }
+
   return (
     <rect
+      fillOpacity={selected ? 1 : 0.5}
       fill={layer.color}
       width="80"
       height="40"
       x={layer.x}
       y={layer.y}
+      onClick={onClick}
     ></rect>
   )
 }
